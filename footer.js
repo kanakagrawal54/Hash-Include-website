2d244f7d8124e22ade4f50e52d5299f611f864ed
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-03-08 18:22:12 +0530

    footer completed

--- footer.js
+++ footer.js
@@ -19,22 +19,23 @@ function Footer() {
             <div className="tieupheading">
                       <h3>Our Partners / Tie-Ups</h3>
                </div>
-              <br/>
+              
                <div className="tieupicons">
-                 <div className="images">
-                     <img src={hackerEarth} width="150px" height="100px"/>
-                 </div>
+                
                 <div className="images">
-                       <img src={Mozilla} width="150px" height="100px"/>
+                       <img src={Mozilla} width="150px" height="130px"/>
                 </div>
                  <div className="images images1">
-                      <img src={codeChef} width="150px" height="100px"/>
+                      <img src={codeChef} width="150px" height="130px"/>
+                </div>
+                 <div className="images img2">
+                     <img src={hackerEarth} width="150px" height="130px"/>
+                 </div>
+                 <div className="images">
+                       <img src={interviewBit} width="150px" height="130px"/>
                 </div>
                  <div className="images">
-                       <img src={interviewBit} width="150px" height="100px"/>
-                </div>
-                 <div className="images">
-                       <img src={Microsoft} width="150px" height="100px"/>
+                       <img src={Microsoft} width="150px" height="130px"/>
                 </div>
                 
               </div>   
@@ -57,9 +58,10 @@ function Footer() {
 
                   <h2>Start a Conversation
                   <hr align="left"/></h2>
+                  <i class="glyphicon glyphicon-envelope"></i>
                   <a class="mail-id" style={{fontSize:"18px"}} href="mailto:#0">contactus@includesgsits.tech</a>
                   <br/>
-                  <a class="info-phone" style={{fontSize:"18px",color:"black"}} href="tel:+919630494120">+91 9630494120</a>
+                  <a class="fa fa-phone" style={{fontSize:"18px",color:"black"}} href="tel:+918085052728">+91 8085052728</a>
                   <br/>
                 </div>
 
@@ -71,9 +73,9 @@ function Footer() {
             <p>&copy; #include SGSITS Indore</p>
             </div>
             <div className="icons">
-            <a class="fa fa-facebook" style={{fontSize:"30px"}} href="#"></a>
-            <a class="fa fa-instagram" style={{fontSize:"30px"}} href="#"></a>
-            <a class="fa fa-linkedin" style={{fontSize:"30px"}} href="#"></a>
+            <a class="fa fa-facebook fam" style={{fontSize:"30px"}} href="https://www.facebook.com/hashincludesgsits"></a>
+            <a class="fa fa-instagram fam" style={{fontSize:"30px"}} href="https://www.instagram.com/include._/"></a>
+            <a class="fa fa-linkedin fam" style={{fontSize:"30px"}} href="https://www.linkedin.com/company/include-sgsits/"></a>
             </div>
             </div>
 
