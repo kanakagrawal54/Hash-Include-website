ad1ea4222aa5a26ffe909937048b346b5f207dd0
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-03-09 15:57:05 +0530

    components made

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import Footer from './footer.js';
+
+import Footer from './OurPartners.js';
+import UpperFooter from './upperfooter.js';
+import Copyright from './copyright.js';
+
 
 function App() {
+
   return (
+  	<div>
     <Footer/>
+    <UpperFooter/>
+    <Copyright/>
+    </div>
   );
+
 }
+
+
 
 export default App;
