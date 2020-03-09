e0e8d24ea2bf400da608b3b38c9cdf0a94870433
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-03-09 16:01:03 +0530

    copyright footer added

---
+++ copyright.js
@@ -0,0 +1,20 @@
+  import React from "react";
+import './copyright.css';
+
+function Copyright(){
+	return(
+  <div className='lower'>
+  		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <meta name="viewport" content="width=device-width, intial-scale=1"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>	
+  		
+              <div className="copyright">
+              <p>&copy;  #include SGSITS Indore.</p>
+              </div>
+              
+              </div>
+              );
+}
+export default Copyright;
